54aa15b1f8bad94be919580062fcd8b58f61d8f0
aketchgithub <vickeryace@gmail.com>
2023-06-11 13:28:51 +0300

    Added footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
 		<>
 			<div className="flex flex-col justify-center items-center shadow-lg text-center  p-5 bg-pink-00">
 				<h1 className=" text-pink-500 font-semibold">
-					© 2023 All rights reserved | Built with ❤ at{" "} 
+					© 2023 All rights reserved | Built with ❤ ©  {" "} 
 					<span className="hover:pink font-semibold cursor-pointer">
-						Moringa School {" "} 
+						VEEK{" "} 
 					</span>
 				</h1>
 			</div>
